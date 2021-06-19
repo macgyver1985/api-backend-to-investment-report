18568aa0e4a80639faab1c866efd5f5a9cae4854
macgyver1985 <felipe.augusto.franca@gmail.com>
2021-06-19 01:26:30 -0300

    Inclusão da camada crossCutting e finalização da handler ObtainAllInvestiments.

--- src/InvestimentReport.Application/Services/ObtainAllInvestimentsHandler.cs
+++ src/InvestimentReport.Application/Services/ObtainAllInvestimentsHandler.cs
@@ -6,7 +6,9 @@ using InvestimentReport.Application.DTOs;
 using InvestimentReport.Application.Helper;
 using InvestimentReport.Application.Interfaces.Adapters;
 using InvestimentReport.Application.Interfaces.Services;
+using InvestimentReport.CrossCutting.Interfaces;
 using InvestimentReport.Domain;
+using Newtonsoft.Json;
 
 namespace InvestimentReport.Application.Service
 {
@@ -14,20 +16,28 @@ namespace InvestimentReport.Application.Service
     public sealed class ObtainAllInvestimentsHandler : Handler, IObtainAllInvestimentsHandler
     {
 
+        private readonly ICache cacheAdapter = null;
         private readonly ILogger loggerAdapter = null;
         private readonly IGetInvestiments getInvestimentsAdapter = null;
+        private const string CACHE_KEY = "full-list-of-investments";
+        private const string CACHE_EXPIRE = "0.24:00:00";
 
         public ObtainAllInvestimentsHandler(
+            ICache cacheAdapter,
             ILogger loggerAdapter,
             IGetInvestiments getInvestimentsAdapter
         )
         {
+            if (cacheAdapter == null)
+                throw new ArgumentException("Cache adapter is null.");
+
             if (loggerAdapter == null)
                 throw new ArgumentException("Logger adapter is null.");
 
             if (getInvestimentsAdapter == null)
                 throw new ArgumentException("Get investments adapter is null.");
 
+            this.cacheAdapter = cacheAdapter;
             this.loggerAdapter = loggerAdapter;
             this.getInvestimentsAdapter = getInvestimentsAdapter;
         }
@@ -39,8 +49,9 @@ namespace InvestimentReport.Application.Service
 
             if (disposing)
             {
-                this.loggerAdapter?.Dispose();
-                this.getInvestimentsAdapter?.Dispose();
+                this.cacheAdapter.Dispose();
+                this.loggerAdapter.Dispose();
+                this.getInvestimentsAdapter.Dispose();
             }
 
             disposed = true;
@@ -51,43 +62,131 @@ namespace InvestimentReport.Application.Service
             if (processId == Guid.Empty)
                 throw new ArgumentException("processId is empty.");
 
+            try
+            {
+                string responseCache = await this.cacheAdapter.Obtain(CACHE_KEY);
+
+                if (!string.IsNullOrWhiteSpace(responseCache))
+                    return JsonConvert.DeserializeObject<IList<Investiment>>(responseCache);
+            }
+            catch (Exception ex)
+            {
+                await this.loggerAdapter
+                    .Error<ObtainAllInvestimentsHandler>(
+                        processId,
+                        $"Erro ao tentar obter o cache {CACHE_KEY}.",
+                        ex
+                    );
+            }
+
             List<Investiment> result = new List<Investiment>();
+            bool hasError = false;
 
             await Task.WhenAll(
                 Task.Factory.StartNew(async () =>
                 {
-                    var list = await this.getInvestimentsAdapter.GetOf<DirectTreasureDTO>();
+                    try
+                    {
+                        var list = await this.getInvestimentsAdapter.GetOf<DirectTreasureDTO>();
 
-                    list?.ToList().ForEach(t =>
-                        {
-                            var item = new Investiment();
+                        list?.ToList().ForEach(t =>
+                            {
+                                var item = new Investiment();
 
-                            result.Add(item);
-                        });
+                                result.Add(item);
+                            });
+                    }
+                    catch (Exception ex)
+                    {
+                        hasError = true;
+
+                        await this.loggerAdapter
+                            .Error<ObtainAllInvestimentsHandler>(
+                                processId,
+                                $"Erro na integração para obter os investimento de {nameof(DirectTreasureDTO)}.",
+                                ex
+                            );
+                    }
                 }),
                 Task.Factory.StartNew(async () =>
                 {
-                    var list = await this.getInvestimentsAdapter.GetOf<FixedIncomeDTO>();
+                    try
+                    {
+                        var list = await this.getInvestimentsAdapter.GetOf<FixedIncomeDTO>();
 
-                    list?.ToList().ForEach(t =>
-                        {
-                            var item = new Investiment();
+                        list?.ToList().ForEach(t =>
+                            {
+                                var item = new Investiment();
 
-                            result.Add(item);
-                        });
+                                result.Add(item);
+                            });
+                    }
+                    catch (Exception ex)
+                    {
+                        hasError = true;
+
+                        await this.loggerAdapter
+                            .Error<ObtainAllInvestimentsHandler>(
+                                processId,
+                                $"Erro na integração para obter os investimento de {nameof(FixedIncomeDTO)}.",
+                                ex
+                            );
+                    }
                 }),
                 Task.Factory.StartNew(async () =>
                 {
-                    var list = await this.getInvestimentsAdapter.GetOf<FundsDTO>();
+                    try
+                    {
+                        var list = await this.getInvestimentsAdapter.GetOf<FundsDTO>();
 
-                    list?.ToList().ForEach(t =>
-                        {
-                            var item = new Investiment();
+                        list?.ToList().ForEach(t =>
+                            {
+                                var item = new Investiment();
 
-                            result.Add(item);
-                        });
+                                result.Add(item);
+                            });
+                    }
+                    catch (Exception ex)
+                    {
+                        hasError = true;
+
+                        await this.loggerAdapter
+                            .Error<ObtainAllInvestimentsHandler>(
+                                processId,
+                                $"Erro na integração para obter os investimento de {nameof(FundsDTO)}.",
+                                ex
+                            );
+                    }
                 })
             );
+
+            if (!hasError && result.Any())
+            {
+                string payload = string.Empty;
+
+                try
+                {
+                    TimeSpan expire = TimeSpan.Parse(CACHE_EXPIRE) - DateTime.UtcNow.TimeOfDay;
+
+                    payload = JsonConvert.SerializeObject(result, Formatting.Indented);
+
+                    await this.cacheAdapter?.Register(
+                        CACHE_KEY,
+                        payload,
+                        expire
+                    );
+                }
+                catch (Exception ex)
+                {
+                    await this.loggerAdapter
+                        .Error<ObtainAllInvestimentsHandler>(
+                            processId,
+                            $"Erro ao tentar registrar o cache {CACHE_KEY}.",
+                            ex,
+                            payload
+                        );
+                }
+            }
 
             return result;
         }
